0728a4c7d5185f05abce5a57f3d0b9c6ff41445f
Aurélien Rithner <aurelien.rithner@hes-so.ch>
2023-03-16 16:12:09 +0100

    Finished exercise 8

--- src/exercise8.py
+++ src/exercise8.py
@@ -1,18 +1,25 @@
-from hashlib import sha224
+import hmac
+from hashlib import sha224, pbkdf2_hmac
+from Crypto.Hash import *
 from random import random
+
+from Crypto.Hash import SHA224
+from Crypto.Random import get_random_bytes
 
 from Utility.Utility import *
 
 NUMBER_OF_FILES = 4
 
-plaintext1 = 'He will be known as King'.encode()
-encodedtext1 = sha224(plaintext1).hexdigest().encode()
+key = get_random_bytes(16)
+
+plaintext1 = b'He will be known as King'
+hmac_text1 = hmac.new(key, plaintext1, SHA224)
 
 # Concatenate two bytes objects in a file
-to_file('with_hash_original.bin', encodedtext1, plaintext1)
+to_file('with_hash_original.bin', hmac_text1.digest(), plaintext1)
 
 # Read bytes_1 and bytes_2 in a file
-bytes_1, bytes_2 = from_file('with_hash_original.bin', len(encodedtext1))
+bytes_1, bytes_2 = from_file('with_hash_original.bin', 28)
 print(bytes_1)
 print(bytes_2)
 
@@ -28,8 +35,8 @@ print(str(NUMBER_OF_FILES) + ' files generated\n')
 #Analysing files
 for i in range(NUMBER_OF_FILES):
     name = "with_hash%d"%i
-    bytes_1, bytes_2 = from_file(name, sha224.__sizeof__())
-    if bytes_1 == sha224(bytes_2).hexdigest().encode():
+    bytes_1, bytes_2 = from_file(name, 28)
+    if bytes_1 == hmac.new(key, bytes_2, SHA224).digest():
         print(name + ' is valid')
     else:
         print(name + 'is not valid')
